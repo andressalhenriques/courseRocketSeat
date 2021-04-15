78de701c70635a91db0f35c871bde7046eb17387
andressalhenriques <andressalhenriques@gmail.com>
2021-04-15 20:30:06 -0300

    node course

---
+++ src/model/Category.ts
@@ -0,0 +1,16 @@
+import {v4 as uuidV4 } from 'uuid'
+
+class Category {
+  id?: string
+  createdAt: Date
+  name: string
+  description: string
+
+  constructor() {
+    if (!this.id) {
+      this.id = uuidV4()
+    }
+  }
+}
+
+export { Category }
